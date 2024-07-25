e46cdd776e67105c7bf9a6195cff2c741941bb44
denyslavrentiev <denisoddessa@gmail.com>
2024-07-25 13:16:59 +0200

    TODO added

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-# TODO 1. **Add welcome message to calculator.**
-# TODO 2. **Add empty lines after the calculate function definition.**
-# TODO 3. **Create main function and put code in it**
-# TODO 4. **Add a dunder main**
-# TODO 5. **Add functionality for subtraction**
-# TODO 6. **Add functionality for multiplication**
-# TODO 7. **Add functionality for division**
-# TODO 8. **Add functionality to ask user to do multiple calculations**
-# TODO 9. **Improve the README.md file and add project description**
+
 
 def calculate(text_input):
     if text_input[1] == "+":
