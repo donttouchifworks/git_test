41ae15610caa608b4889607f13deea2caf3a1c47
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-07-25 13:10:07 +0200

    add welcome screen to CLI

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def calculate(text_input):
     if text_input[1] == "+":
         return int(text_input[0])+int(text_input[-1])
-calculation = input("What do you want to calculate?")
-answer = calculate(calculation)
-print(f"The result is {answer}")
+
+def main():
+    print("Welcome to the calculator")
+    calculation = input("What do you want to calculate?")
+    answer = calculate(calculation)
+    print(f"The result is {answer}")
+
+if __name__ == "__main__":
+    main()
